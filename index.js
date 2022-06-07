4d154cca9260a36d94250c617d2276bc18dd14be
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-06-07 19:45:18 +0300

    feat: Invalid input

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import readline from 'readline';
+import path from 'path';
 
+const __dirname = path.resolve();
 const args = process.argv.slice(2);
 const input = process.stdin;
 const output = process.stdout;
@@ -9,13 +11,15 @@ const entrance = async () => {
     const username = args.find((e) => e.includes('--username=')).replace('--username=', '');
     const welcome = `Welcome to the File Manager, ${username}!\n`;
     output.write(welcome);   
+    output.write(`You are currently in ${__dirname}\n`);
     rl.on('line', (input) => {       
         switch (input) {
             case '.exit':
+                output.write(`Thank you for using File Manager, ${username}!`); 
                 rl.close();
-                break;
-        
+                break;                      
             default:
+                output.write('Invalid input\n');
                 break;
         }
     }); 
