8f41ac52473e035ebca923c7465714a40cc2d2a5
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-06-11 18:14:46 +0300

    refactor: accessPath.js

--- commands/accessPath.js
+++ commands/accessPath.js
@@ -1,32 +1,10 @@
 import fs from 'fs';
-import { constants } from 'fs';
 const fsp = fs.promises;
-/*
-export const accessPath = (pathFile) => {
-  return new Promise((resolve, reject) => {
-    return fsp.access(pathFile, constants.F_OK | constants.W_OK, (err) => {
-      if(!err) return resolve(true);      
-      if(err) return resolve(false);      
-    });
-  });
-}
-
-/*
-export const accessPath = (pathFile) => {
-  try {
-    fsp.access(pathFile, constants.F_OK | constants.W_OK, (err) => {
-      if (err.code === 'ENOENT') return true;
-      return false;
-    });
-  } catch (error) {
-    console.log(`${error}`);
-  }  
-}
-*/
 
 export const accessPath = async (pathFile) => {
   try {
     return await fsp.stat(pathFile);    
-  } catch (err) {    
+  } catch (err) { 
+    return undefined;   
   } 
 }
