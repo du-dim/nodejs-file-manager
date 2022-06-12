1de10dade22868e62f6ba53fcb1fcc39cd9b2df3
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-06-12 23:53:28 +0300

    feat: command os

--- commands/os/os.js
+++ commands/os/os.js
@@ -1,1 +1,40 @@
-export const osFunc = () => console.log('OperationList: os');
+import os from 'os';
+
+export const osFunc = async (text) => {
+  try {
+    if (text.trim().length < 1) throw Error('\x1b[35mInvalid input\x1b[0m');    
+    const codeSplit = text.split('--');
+    const code = codeSplit.join('');
+    const result = {EOL, cpus, homedir, username, architecture};
+    const check = Object.keys(result).includes(code); 
+    
+    if (codeSplit.length > 1) throw Error('\x1b[35mInvalid input\n\x1b[0m');
+    if (codeSplit.length > 1) throw Error('\x1b[35mInvalid input\n\x1b[0m');
+    if (!check) throw Error('\x1b[35mInvalid input\n\x1b[0m');
+
+    const resultCPU = [];
+    const cpus = os.cpus();  
+    const amountCPUS = `\x1b[36mTotal number of CPUs:\x1b[33m ${os.cpus().length}\n\x1b[0m`;
+
+    let count = 0;
+    for await (const item of cpus) {
+      count++;    
+      const cpu_No = count; 
+      const model= item.model;
+      const speed = `${item.speed/1000} GHz`;
+      const objCPU = { cpu_No, model, speed };
+      resultCPU.push(objCPU);    
+    }  
+    
+      result = {
+      EOL: [amountCPUS, JSON.stringify(os.EOL)],
+      cpus: resultCPU,
+      homedir: os.homedir(),
+      username: os.userInfo().username,
+      architecture: os.arch(),
+    }     
+  } catch (error) {
+    console.log(error.message)    
+  }  
+
+};
--- commands/switchCommand.js
+++ commands/switchCommand.js
@@ -34,7 +34,8 @@ export const switchCommand = async (dirname, command) => {
         await commands.rmFunc(dirname, command.slice(1).join(' '));                                 
         break;
     case 'os':
-        commands.osFunc();                                 
+        await commands.osFunc(command.slice(1).join(' ')); 
+        dirname;                                 
         break;
     case 'hash':
         await commands.hashFunc(dirname, command.slice(1).join(' '));                                 
@@ -48,38 +49,10 @@ export const switchCommand = async (dirname, command) => {
     case 'y':                 
           break          
     case 'n':             
-      break          
-    case 'link':
-        await pathToFileDir(dirname, command.slice(1).join(' '));                                 
-        break;
+      break;   
     default:
         process.stdout.write('\x1b[35mInvalid input\n\x1b[0m');                
         break;
   }   
   return newDirname
 }
-
-
-/*
-export const switchCommand = async (dirname, command) => { 
-    const objSwitch = {
-        help: commands.helpFunc(),
-        up: await commands.cdFunc(dirname, '..'),
-        cd: await commands.cdFunc(dirname, command.slice(1).join(' ')),
-        ls: await commands.lsFunc(dirname),
-        cat: await commands.catFunc(dirname, command.slice(1).join(' ')),
-        add: await commands.addFunc(dirname, command.slice(1).join(' ')),
-        rn: await commands.rnFunc(dirname, command.slice(1).join(' ')),
-        cp: await commands.cpFunc(dirname, command.slice(1).join(' ')),
-        mv: await commands.cpFunc(dirname, command.slice(1).join(' ')),
-        rm: await commands.rmFunc(dirname, command.slice(1).join(' ')),
-        os: await commands.osFunc(),
-        hash: await commands.hashFunc(),
-        compres: await commands.compressFunc(),
-        decomress: await commands.decompressFunc(), 
-    }
-    const choese = objSwitch[command[0]];
-    console.log(choese);
-    return choese;
-}
-*/
